cc0d0bbc951ae2e60ea26e7835520b1e30bf7c32
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-06-25 17:40:58 +0100

    Updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'https://proj4-webscrapper-ws-deploy-objszsdm2-pin7i7os-projects.vercel.app/api/',
+  api_url: 'https://proj4-webscrapper-ws-deploy.vercel.app/api/',
   session: {
     TOKEN_KEY: 'user-token'
   }
